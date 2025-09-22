35c85b87a09ebe66336e0fc71c83885db69e14b6
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2025-09-22 21:20:56 +0200

    J'ai enlever le code fait en langage Mbed

--- src/main.cpp
+++ src/main.cpp
@@ -58,32 +58,3 @@ void loop()
     }
 }
 
-// #include <mbed.h>
-// #include "STEPPER.h"
-// // CAN can(PA_11, PA_12, 1000000);  // CAN Rx pin name, CAN Tx pin name
-// DigitalOut led(LED1);
-
-// int main()
-// {
-//     double temps = 0;
-//     initStepper();
-//     printf("CRAC stm32f303k8 démo\n");
-
-//     while (true)
-//     {
-//         stepper(convert_angle_to_pas(90), PAS_COMPLET, 0);
-
-//         ThisThread::sleep_for(250ms);
-//         led = 0;
-//         ThisThread::sleep_for(250ms);
-//         led = 1;
-//         stepper(convert_angle_to_pas(-90), PAS_COMPLET, 0);
-//         ThisThread::sleep_for(250ms);
-//         led = 0;
-//         ThisThread::sleep_for(250ms);
-//         led = 1;
-//         printf("%.1f\n", temps);
-
-//         temps += 0.5;
-//     }
-// }
